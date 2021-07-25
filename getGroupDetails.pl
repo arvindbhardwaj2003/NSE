6dd90250cb7f658f3aac6cb171e2139b69b543ee
arvind <arvindbhardwaj2003@algosec.com>
2021-07-25 21:05:12 +0300

    Getting Group Details

--- getGroupDetails.pl
+++ getGroupDetails.pl
@@ -1,1 +1,21 @@
 ## This code is to get details of All group from device using SOAP API
+no warnings qw(redefine);
+use strict;
+
+use lib qw(/usr/share/fireflow/local/etc/site/lib);
+
+use AFAWebServicesClient;
+use Data::Dumper;
+
+my $user = 'admin';
+my $pass = 'algosec';
+
+print "PS Custom Report:  Generation Begins \n";
+
+my $sessionId = AFAWebServicesClient::ConnectAFA("$user","$pass");
+
+print "PS Custom Report:  Session Id generated - $sessionId \n";
+
+my @groupName = AFAWebServicesClient::GetHostGroupsRequest($sessionId,'Bamba');
+
+print Dumper(@groupName);
